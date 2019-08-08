be043068278b5e31f7e5aa65c295ad004ddf0ed0
Anthony Rey <anthony.rey5@gmail.com>
2019-08-08 14:27:41 +0200

    added a start of inventory

---
+++ InventorySlot.gd
@@ -0,0 +1,20 @@
+extends Control
+class_name InventorySlot
+
+func get_drag_data(position):
+	if get_child_count() == 0:
+		return null
+	var obj = get_children()[0]
+	var preview = TextureRect.new()
+	preview.texture = obj.texture
+	set_drag_preview(preview)
+	return obj
+
+func can_drop_data(position, data):
+	assert(get_child_count() <= 1)
+	return get_child_count() == 0
+
+func drop_data(position, data):
+	data.show()
+	data.get_parent().remove_child(data)
+	add_child(data)
--- scenes/scripts/Player/PlayerInventory.gd
+++ scenes/scripts/Player/PlayerInventory.gd
@@ -1,6 +1,20 @@
-extends Control
+extends Popup
+
+func _ready():
+	##$InventoryPopup/Bag.connect("mouse_entered", self, "_on_mouse_entered")
+	##$InventoryPopup/Bag.connect("mouse_exited", self, "_on_mouse_exited")
+	pause_mode = PAUSE_MODE_PROCESS
 
 func show_inventory():
 	get_tree().paused = true
-	get_tree().paused = false
-	pass
+	show()
+	.popup()
+	
+func can_drop_data(position, data):
+	return true
+	
+func drop_data(position, data):
+	if !$Bag.get_rect().has_point(position):
+		data.get_parent().remove_child(data)
+		print("dropped sword")
+		data.free()
